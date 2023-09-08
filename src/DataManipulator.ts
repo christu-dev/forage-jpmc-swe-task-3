c24077fde1e649ea73cfa3ecaf0fadf0c3a397ea
christu-dev <ct@christu.dev>
2023-09-08 11:45:08 -0400

    Update Graph component and DataManipulator class

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,39 @@
 import { ServerRespond } from './DataStreamer';
 
+//Row interface matches schema in Graph component
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  stockprice_abc: number,
+  stockprice_def: number,
+  ratio: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
   timestamp: Date,
 }
-
+//this interface determines structure of the object returned by generateRow
+//the return object must correspond to the schema in Graph
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+  static generateRow(serverRespond: ServerRespond[]) : Row {
+    //calculating stock prices as the avg of top ask and bid (like in task 1)
+    //serverRespond is accessible as an array, first elem is stock ABC's data
+    const price_ABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const price_DEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = price_ABC / price_DEF;
+    //const upperBound = 1 + 0.10; //+10% of 12 month historical avg
+    //const lowerBound = 1 - 0.10; //-10% of 12 month historical avg
+    const upperBound = 1 + 0.03; 
+    const lowerBound = 1 - 0.03;
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        stockprice_abc: price_ABC,
+        stockprice_def: price_DEF,
+        ratio,
+        
+        upper_bound: upperBound,
+        lower_bound: lowerBound,
+        trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
+        timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
+          serverRespond[0].timestamp : serverRespond[1].timestamp,
       };
-    })
-  }
+  } //return value is a single Row Object
 }
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import { Table, TableData } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -22,10 +22,13 @@ class Graph extends Component<IProps, {}> {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
-    const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+    const schema = { //declarative format for describing the structure of data, written in JSON
+      stockprice_abc: 'float',
+      stockprice_def: 'float',
+      ratio: 'float', //to look for correlation weakening
+      upper_bound: 'float', //+10%
+      lower_bound: 'float', //-10%
+      trigger_alert: 'float', //to check when bounds are crossed
       timestamp: 'date',
     };
 
@@ -35,24 +38,28 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
-      elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
-      elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+      elem.setAttribute('view', 'y_line'); //line graph view
+      elem.setAttribute('row-pivots', '["timestamp"]'); //datapoints marked by time
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]'); //horizontal upper and lower bounds, a moving ratio, and a trigger alert
+      //column pivots no longer needed, no need to distinguish the stocks
+      elem.setAttribute('aggregates', JSON.stringify({ //average values of data point on same timestamps
+        stockprice_abc: 'avg',
+        stockprice_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
       }));
     }
   }
 
-  componentDidUpdate() {
+//lifecycle method that updates when graph gets new data
+  componentDidUpdate() { 
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
+      ] as unknown as TableData);
     }
   }
 }
